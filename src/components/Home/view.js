d8519bd27ad5e493eca838feb2b69e55eac8180f
Subash Pradahan <suprowilde5@gmail.com>
2020-03-30 11:14:45 +0200

    Working on design home-page

--- src/components/Home/view.js
+++ src/components/Home/view.js
@@ -1,16 +1,32 @@
 import React from 'react'
+import '../../styles/home.css'
 
 export default function view(props) {
   const { data } = props
   const totals = data.map(numbers =>
-    <div key={data.length}>
-      <p>Global Confirmed Number : {numbers.confirmed}</p>
-      <p>Global Recovered Number : {numbers.recovered}</p>
-      <p>Global: {numbers.deaths}</p>
+    <div key={data.length} align='center'>
+      <div className='row'>
+        <div className='numbers col-6' >
+          Global Confirmed Number : <p> {numbers.confirmed}</p>
+        </div>
+
+        <div className='numbers col-6'>
+          Global Recovered Number :
+          <p>{numbers.recovered}</p>
+        </div>
+      </div>
+
+      <div className='row'>
+        <div className='numbers col-12'>
+          Global Deaths:<p> {numbers.deaths}</p>
+        </div>
+      </div>
     </div>
   )
-  return <div align='center'>
-      <h1>CORONA UPDATES</h1>
-    {totals}
+  return <div className='container'>
+    <h1 align='center'>COVID- 19 (CORONA) UPDATES</h1>
+    <div className='numbers-container'>
+      {totals}
+    </div>
   </div>
 }
--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,2 @@
 export const getTotals = 'https://covid-19-data.p.rapidapi.com/totals'
+// export const getTotals = 'https://api.covid19api.com/all'
