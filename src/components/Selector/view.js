a1227979cfe6976c9b4645f6f9c98d390f05f1dc
Subash Pradahan <suprowilde5@gmail.com>
2020-04-03 20:07:09 +0200

    Checking for style

--- src/components/Selector/view.js
+++ src/components/Selector/view.js
@@ -7,7 +7,6 @@ export default function view(props) {
   const { onSelectFlag } = props
   return <div className='data-container'>
     <ReactFlagsSelect
-      refs="userFlag"
       className='selector'
       searchable={true}
       searchPlaceholder="Search for a country"
