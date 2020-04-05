0fd420ea497df13208484ca283e769885955e694
Subash Pradahan <suprowilde5@gmail.com>
2020-04-05 10:52:31 +0200

    adding conditions for rendering

--- src/components/GlobeData/index.js
+++ src/components/GlobeData/index.js
@@ -4,12 +4,19 @@ import { connect } from 'react-redux'
 import View from './view'
 
 class GlobeData extends Component {
+  state= {isLoading: true}
+
   componentDidMount() {
     this.props.fetchData()
+    this.setState({
+      isLoading: false
+    })
   }
 
   render() {
-    return <View data={this.props.data} />
+    return <View data={this.props.data} 
+      isLoading={this.state.isLoading}
+    />
   }
 }
 
--- src/components/GlobeData/view.js
+++ src/components/GlobeData/view.js
@@ -4,47 +4,51 @@ import '../../styles/loading.css'
 import AnimatedNumber from "animated-number-react";
 
 export default function view(props) {
-  const { data } = props
+  const { data, isLoading } = props
+
   const formatValue = (value) => {
-    if(!value){
+    if (!value) {
       return <span className='loading'></span>
     }
     return `${Number(value).toFixed(0)}`;
   }
-  
-  return <div className='data-container container'>
-    <div className='numbers-container'>
-      <div className='row'>
-        <div className='numbers col-lg-4 col-12 confirmed' >
-          Global Confirmed:
+  if (!isLoading) {
+    return <div className='data-container container'>
+      <div className='numbers-container'>
+        <div className='row'>
+          <div className='numbers col-lg-4 col-12 confirmed' >
+            Global Confirmed:
           <p className='total'>
-            <AnimatedNumber
-              value={data.cases}
-              formatValue={formatValue}
-            />
-          </p>
-        </div>
-
-        <div className='numbers col-lg-4 col-12 recovered'>
-          Global Recovered:
-          <p className='total'>
-            <AnimatedNumber
-              formatValue={formatValue}
-              value={data.recovered}
+              <AnimatedNumber
+                value={data.cases}
+                formatValue={formatValue}
               />
-          </p>
-        </div>
+            </p>
+          </div>
 
-        <div className='numbers col-lg-4 col-12 deaths'>
-          Global Deaths:
+          <div className='numbers col-lg-4 col-12 recovered'>
+            Global Recovered:
           <p className='total'>
-            <AnimatedNumber
-              value={data.deaths}
-              formatValue={formatValue}
-            />
-          </p>
+              <AnimatedNumber
+                formatValue={formatValue}
+                value={data.recovered}
+              />
+            </p>
+          </div>
+
+          <div className='numbers col-lg-4 col-12 deaths'>
+            Global Deaths:
+          <p className='total'>
+              <AnimatedNumber
+                value={data.deaths}
+                formatValue={formatValue}
+              />
+            </p>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  } else {
+    return <div className='loading pageLoading'></div>
+  }
 }
