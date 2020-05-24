8c48955be8fa259db2cef4a0ef99445d617d547f
Subash Pradahan <suprowilde5@gmail.com>
2020-05-24 14:21:43 +0200

    Check deployment

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,10 +3,9 @@ const {createProxyMiddleware} = require('http-proxy-middleware')
 module.exports = app => {
   app.use(
     createProxyMiddleware("/v2", {
-      target: `https://newsapi.org`,
+      target: "http://newsapi.org",
       secure: false,
       changeOrigin: true
-
     })
   )
 }
