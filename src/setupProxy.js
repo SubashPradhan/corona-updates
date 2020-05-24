035030bdd7e5be2d4bc67df5ad805cb769bb2926
Subash Pradahan <suprowilde5@gmail.com>
2020-05-24 17:38:26 +0200

    Checking redirects

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const {createProxyMiddleware} = require('http-proxy-middleware')
 
 module.exports = app => {
   app.use(
-    createProxyMiddleware("/v2", {
+    createProxyMiddleware("/v2/top-headlines", {
       target: "http://newsapi.org",
       secure: false,
       changeOrigin: true
