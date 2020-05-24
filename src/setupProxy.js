9ee4826550c454629d9498cfda7fa1ce58a98bde
Subash Pradahan <suprowilde5@gmail.com>
2020-05-24 23:36:24 +0200

    Cors config for netlify

---
+++ src/setupProxy.js
@@ -0,0 +1,10 @@
+const {createProxyMiddleware} = require('http-proxy-middleware')
+
+module.exports = app => {
+  app.use(
+    createProxyMiddleware("/v2/top-headlines", {
+      target: "http://newsapi.org",
+      changeOrigin: true
+    })
+  )
+}
